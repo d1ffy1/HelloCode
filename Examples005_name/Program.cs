26aae2e25226a91beaa559f043376b2ea8def633
Vadim Stryukan <vadimstryukan@gmail.com>
2023-02-25 16:47:08 +0200

    made example

---
+++ Examples005_name/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "анита")
+{
+    Console.WriteLine($"Ура, это же {username}!");
+}
+else
+{
+    Console.WriteLine($"Привет, {username}!");
+}
