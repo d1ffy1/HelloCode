79e4b04a48263799385c4bf2c52952e1cf3adae9
Vadim Stryukan <vadimstryukan@gmail.com>
2023-03-03 14:13:12 +0200

    after 2nd lection

---
+++ Examples009/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1; 
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {11, 21, 31, 41, 15, 16, 17, 18, 19};
+
+
+Console.WriteLine(array[4]);
+int index = 0;
+
+int max = Max(Max(
+    while (index < 9)
+    {
+        max = 
+    }
+));
+
+Console.WriteLine(max);
